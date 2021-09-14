6765c7a4305aaf6ce28bb6c39c42297ca8faee5e
Jiaguo Tang <1205322591@qq.com>
2021-09-14 23:32:04 +0800

    添加新内容

--- 链表/反转链表2.py
+++ 链表/反转链表2.py
@@ -2,7 +2,7 @@
 Description: 疑问
 Author: Tjg
 Date: 2021-06-09 14:48:25
-LastEditTime: 2021-07-26 11:01:28
+LastEditTime: 2021-09-10 11:49:51
 LastEditors: Please set LastEditors
 '''
 # Definition for singly-linked list.
@@ -20,46 +20,6 @@ class ListNode:
             p = p.next
         return "->".join(print_string)
 
-# 两次遍历
-class Solution:
-    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        def reverse_linked_list(head: ListNode):
-            # 也可以使用递归反转一个链表
-            pre = None # 从None开始
-            cur = head
-            while cur:
-                next = cur.next
-                cur.next = pre
-                pre = cur
-                cur = next
-
-        # 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
-        dummy_node = ListNode(-1)
-        dummy_node.next = head
-        pre = dummy_node
-        # 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
-        # 建议写在 for 循环里，语义清晰
-        for _ in range(left - 1):
-            pre = pre.next
-
-        # 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
-        right_node = pre
-        for _ in range(right - left + 1):
-            right_node = right_node.next
-        # 第 3 步：切断出一个子链表（截取链表）
-        left_node = pre.next
-        curr = right_node.next
-
-        # 注意：切断链接
-        pre.next = None
-        right_node.next = None
-
-        # 第 4 步：同第 206 题，反转链表的子区间
-        reverse_linked_list(left_node)
-        # 第 5 步：接回到原来的链表中
-        pre.next = right_node
-        left_node.next = curr
-        return dummy_node.next
 
 # 一次遍历
 class Solution:
@@ -69,14 +29,22 @@ class Solution:
         pre = dummy_node
         for _ in range(left - 1):
             pre = pre.next
-
+        # pre是前一半链表最后一个节点，不用变
+        # cur是正在反转的链表中最后一个节点，也不用变
+        # next是即将加入反转链表的节点
         cur = pre.next
         for _ in range(right - left):
+            # 获取即将加入反转链表的节点
             next = cur.next
+            # 将反转链表中最后一个节点指向新加入节点的下一个节点
             cur.next = next.next
+            # 新加入的节点指向反转链表中第一个节点
             next.next = pre.next
+            # 前一半链表最后一个节点指向新加入节点
             pre.next = next
+
         return dummy_node.next
+
 
 l1 = ListNode(1)
 l1.next = ListNode(2)
