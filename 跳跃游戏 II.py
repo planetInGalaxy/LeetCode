65f24ae929e1bdb30b7d9e78a98e5c54c320f01a
Jiaguo Tang <1205322591@qq.com>
2022-01-26 00:17:56 +0800

    1+2

--- 跳跃游戏 II.py
+++ 跳跃游戏 II.py
@@ -2,25 +2,45 @@
 Description: 
 Author: Tjg
 Date: 2021-01-30 16:38:41
-LastEditTime: 2021-01-30 16:48:40
+LastEditTime: 2022-01-26 00:05:54
 LastEditors: Please set LastEditors
 '''
+# 贪心
+# 时间复杂度O(n)
 class Solution(object):
     def jump(self, nums):
         if len(nums) == 1:
             return 0
-        count = 1
+        count = 0
         i = 0
-        while i + nums[i] < len(nums) - 1:
+        while i < len(nums) - 1:
             max_step = 0
-            max_index = 0
+            max_index = i
             for j in range(i + 1, i + nums[i] + 1):
+                if j >= len(nums) - 1:
+                    count += 1
+                    return count
                 if max_step <= j - i + nums[j]:
                     max_step = j - i + nums[j]
                     max_index = j
             i = max_index
             count += 1
         return count
+
+# 贪心
+# 时间复杂度O(n)
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+        n = len(nums)
+        maxPos, end, step = 0, 0, 0
+        for i in range(n - 1):
+            if maxPos >= i:
+                maxPos = max(maxPos, i + nums[i])
+                if i == end:
+                    end = maxPos
+                    step += 1
+        return step
+
 
 s = Solution()
 ls = [2,3,1,1,4]
--- 跳跃游戏.py
+++ 跳跃游戏.py
@@ -1,27 +1,22 @@
 '''
 Description: 
+给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+判断你是否能够到达最后一个下标。
 Author: Tjg
 Date: 2021-01-30 15:51:58
-LastEditTime: 2021-01-30 16:38:29
+LastEditTime: 2022-01-25 23:33:58
 LastEditors: Please set LastEditors
 '''
-class Solution(object):
+# 贪心
+# 时间复杂度O(n)
+class Solution:
     def canJump(self, nums):
-        if len(nums) == 1:
-            return True
-        i = 0
-        while True:
-            if i + nums[i] >= len(nums) - 1:
-                return True
-            elif nums[i] == 0:
-                return False
-            max_step = 0
-            max_index = 0
-            for j in range(i + 1, i + nums[i] + 1):
-                if max_step <= j - i + nums[j]:
-                    max_step = j - i + nums[j]
-                    max_index = j
-            i = max_index
+        max_i = 0  # 初始化当前能到达最远的位置
+        for i, jump in enumerate(nums):  # i为当前位置，jump是当前位置的跳数
+            if max_i >= i and i+jump > max_i:  # 如果当前位置能到达，并且当前位置+跳数>最远位置
+                max_i = i+jump  # 更新最远能到达位置
+        return max_i >= i
 
 s = Solution()
 ls = [2,0,0]
